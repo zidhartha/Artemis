d62c2b7382342196f0bb3ce086075a7ddf33da7e
dato <Jincharadze.Davit@kiu.edu.ge>
2024-03-28 22:40:15 +0400

    Create Main.java

---
+++ Doubly Linked Lists/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] args) {
+        IntDoubleList lst = null;
+        lst.append(7);
+        lst.append(12);
+        String t = lst.toString();
+        System.out.print(t);
+
+    }
+}
