1f8d4f0d6e3d1364da8d460a5072083caecdfaf7
dato <Jincharadze.Davit@kiu.edu.ge>
2024-03-28 22:21:41 +0400

    Create Future.java

---
+++ Thread Pools/Future.java
@@ -0,0 +1,18 @@
+
+
+public class Future {
+
+	private boolean finished = false;
+
+	public synchronized void get() throws InterruptedException {
+		while (!finished) {
+			wait();
+		}
+	}
+
+	public synchronized void finish() {
+		finished = true;
+		notifyAll();
+	}
+
+}
